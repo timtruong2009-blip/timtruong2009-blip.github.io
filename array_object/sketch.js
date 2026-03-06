3d60d7f1c9cbd68532f14c7484bdd3065d69d545
7006116-cloud <7006116@spsd.sk.ca>
2026-03-06 15:25:09 -0600

    update array

--- array_object/sketch.js
+++ array_object/sketch.js
@@ -5,12 +5,31 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let pos = {x: 0, y: 0};
+
+function preload() {
+  // connect to a p5party server
+  partyConnect(
+    "wss://demoserver.p5party.org",
+    "adawdf af wawd",
+  );
+  
+  // tell p5.party to sync the pos object
+  pos = partyLoadShared("pos", pos);
+  
+}
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(400, 400);
 }
 
 function draw() {
-  background(220);
-  plane();
+  background(50);
+  ellipse(pos.x, pos.y, 100, 100);
 }
+
+function mousePressed() {
+  pos.x = mouseX;
+  pos.y = mouseY;
+}
+
