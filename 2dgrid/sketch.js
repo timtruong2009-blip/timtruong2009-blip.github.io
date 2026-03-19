9cb4e4660fcf4a4a2150ad0a13f0322472f7ad76
timtruong2009-blip <tim.truong.2009@gmail.com>
2026-03-19 13:05:10 -0600

    update grid

--- 2dgrid/sketch.js
+++ 2dgrid/sketch.js
@@ -5,17 +5,32 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let haha = [[1,0,0,0],
-  [1,0,1,0],
-  [0,1,0,0],
-  [0,0,1,1]
-];
-let SQUAREDIMENSION = haha.length;
+// use this if hard code
+// let haha = [[1,0,0,0],
+//   [1,0,1,0],
+//   [0,1,0,0],
+//   [0,0,1,1]
+// ];
+
+// let SQUAREDIMENSION = haha.length;
+
+
+// use this if randomizing
+let haha;
+let SQUAREDIMENSION = 10;
 let cellsize;
+let mouse_press_pos;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  cellsize = height/SQUAREDIMENSION;
+  if (width < height){
+    cellsize = width/SQUAREDIMENSION;
+  }
+  else{
+    cellsize = height/SQUAREDIMENSION;
+  }
+  haha = randomGrid(SQUAREDIMENSION, SQUAREDIMENSION);
+
 }
 
 function draw() {
@@ -23,8 +38,8 @@ function draw() {
   show();
 }
 function show(){
-  for (let y = 0; y < SQUAREDIMENSION, y ++;){
-    for (let x = 0; x < SQUAREDIMENSION, x ++;){
+  for (let y = 0; y < SQUAREDIMENSION; y ++){
+    for (let x = 0; x < SQUAREDIMENSION; x ++){
       if (haha[y][x] === 0){
         fill("white");
       }
@@ -34,4 +49,30 @@ function show(){
       square(x * cellsize,y * cellsize, cellsize);
     }
   }
+}
+
+function mousePressed(){
+  mouse_press_pos = {x : floor(mouseX / cellsize), y : floor(mouseY/ cellsize)};
+  if (haha[mouse_press_pos.y][mouse_press_pos.x] === 1){
+    haha[mouse_press_pos.y][mouse_press_pos.x] = 0;
+  }
+  else{
+    haha[mouse_press_pos.y][mouse_press_pos.x] = 1;
+  }
+}
+
+function randomGrid(col, row){
+  let array = [];
+  for (let y = 0; y < row; y ++){
+    array.push([]);
+    for (let x = 0; x < col; x++){
+      if (random(100) < 50){
+        array[y].push(1);
+      }
+      else{
+        array[y].push(0);
+      }
+    }
+  }
+  return array;
 }
