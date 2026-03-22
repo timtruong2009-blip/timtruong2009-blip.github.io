9a9811d25c98c8a894bdc354bbff1a2e38822f99
timtruong2009-blip <tim.truong.2009@gmail.com>
2026-03-21 21:32:16 -0600

    finding character for 2d grid

--- 2dgrid/sketch.js
+++ 2dgrid/sketch.js
@@ -4,30 +4,65 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-
-// use this if hard code
-// let haha = [[1,0,0,0],
-//   [1,0,1,0],
-//   [0,1,0,0],
-//   [0,0,1,1]
-// ];
-
-// let SQUAREDIMENSION = haha.length;
+//
+// Credit: 
+// - Vagabond character: pixramen on Itch.io
+// - Warrior character: Fixels on Itch.io
+// - School girl character: Free Game Assets on Itch.io
+// - Mage character: LuizMelo on Itch.io
 
 
 // use this if randomizing
-let haha;
-let SQUAREDIMENSION = 10;
-let cellsize;
-let mouse_press_pos;
+const WORLDSIZE = 4000;
+const GRIDSIZE = 20;
+let theWorld = [];
+
+let vagabondStarterPack;
+
+
+function preload(){
+  vagabondStarterPack = loadImage("Characters/vagabond/vagabond-idle.gif");
+}
 
 function setup() {
+  makeWorld();
+  console.log(theWorld);
+  noStroke();
   createCanvas(windowWidth, windowHeight);
-
+  noSmooth();
+  // for (let y = 0; y < theWorld.length; y ++){
+  //   for (let x = 0; x < theWorld[y].length; x ++){
+  //     if (theWorld[y][x] === 1){
+  //       fill("black");
+  //     }
+  //     else{
+  //       fill("white");
+  //     }
+  //     square(x * GRIDSIZE,y* GRIDSIZE, GRIDSIZE);
+  //   }
+  // }
 }
+
 
 function draw() {
-  background(220);
-
+  image(vagabondStarterPack,0,0);
 }
+
+function makeWorld(){
+  for (let y = 0; y <= WORLDSIZE; y ++){
+    let row_x = [];
+    for (let x = 0; x <= WORLDSIZE; x ++){
+      if (y === 1 || y === WORLDSIZE -1){
+        row_x.push(1);
+      }
+      else{
+        row_x.push(0);
+      }
+    }
+    theWorld.push(row_x);
+  }
+}
+
+
+
 
